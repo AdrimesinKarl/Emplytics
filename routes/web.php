6912af22b7d19d20c3e6ac6d55d9f3421689cb0d
AdrimesinKarl <jcarlo1002@gmail.com>
2026-02-14 21:48:39 +0800

    added authentication scaffolding using laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
-use Livewire\Volt\Volt;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\AttendanceController;
-use App\Http\Controllers\PayrollController;
-  //I've got some errors here because I didn't import the EmployeeController when I created the route. I've fixed it by adding this line.
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
-    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
-    Volt::route('settings/password', 'settings.password')->name('user-password.edit');
-    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
-
-    Volt::route('settings/two-factor', 'settings.two-factor')
-        ->middleware(
-            when(
-                Features::canManageTwoFactorAuthentication()
-                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-                ['password.confirm'],
-                [],
-            ),
-        )
-        ->name('two-factor.show');
-
-Route::resource('employees',EmployeeController::class); //this is the route to list all employees
-
-Route::resource('attendances', AttendanceController::class)->middleware('auth'); //this is the route to list all attendances
-
-Route::resource('payrolls', PayrollController::class)->middleware('auth'); //this is the route to list all payrolls
-
-});
-
+require __DIR__.'/auth.php';
